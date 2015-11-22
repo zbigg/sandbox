b9f167a1845b6691c00aab0cc42d6156e137e6c5
zbigg <zbigg@spotbeans.com>
2015-11-23 00:33:10 +0100

    async_sandbox: memoize result, fix future<void>, support multiple then calls

--- async_sandbox.cpp
+++ async_sandbox.cpp
@@ -6,6 +6,9 @@
 #include <string>
 #include <cassert>
 #include <condition_variable>
+#include <functional>
+#include <memory>
+#include <algorithm>
 
 //
 //
@@ -33,12 +36,92 @@ template <typename T, typename F>
 future<T> async(F fun);
 
 //
+// optional
+//   naive impl based on unique_ptr
+// TBD, use some real implementation
+//
+
+template <class T>
+class optional {
+    std::unique_ptr<T> v;
+public:
+    optional(): v(nullptr) {}
+    optional(T&& second): v(new T(second)) {} // TBD, should it be explicit ?
+    optional(optional<T> const& second): v( second.get() ? new T(*second.get()) : nullptr) {}
+
+    optional<T>& operator=(optional<T>& second) {
+        optional<T> tmp(*this);
+        using std::swap;
+        swap(*this, second);
+    }
+
+    void     reset() { v.release(); }
+    void     reset(T value) { v.reset(new T(std::move(value))); }
+
+    operator bool() const { return v.get() != nullptr; };
+    bool operator!() const { return v.get() == nullptr; }
+
+    T*       get()       { return v.get(); }
+    T const* get() const { return v.get(); }
+    T&       ref()       { assert(v); return * v.get(); }
+    T const& ref() const { assert(v); return * v.get(); }
+    T*       operator ->()       { return get(); }
+    T const* operator ->() const { return get(); }
+};
+
+template <typename T, typename V>
+bool operator &&(optional<T> const& a, optional<V> const& b ) {
+    return a.get() && b.get();
+}
+
+template <typename T, typename V>
+bool operator ||(optional<T> const& a, optional<V> const& b ) {
+    return a.get() || b.get();
+}
+
+template <typename T, typename V>
+bool operator ==(optional<T> const& a, optional<V> const& b ) {
+    return ( a && b )   ? ( a.ref() == b.ref() ) :
+           ( !a && !b ) ? true
+                        : false;
+}
+
+template <typename T, typename V>
+bool operator !=(optional<T> const& a, optional<V> const& b ) {
+    return ( a && b )   ? ( a.ref() != b.ref() ) :
+           ( !a && !b ) ? false
+                        : true;
+}
+
+template <typename T, typename V>
+bool operator ==(optional<T> const& a, V const& b ) {
+    return a.get() && a.ref() == b;
+}
+
+template <typename T, typename V>
+bool operator !=(optional<T> const& a, V const& b ) {
+    return !a.get() || a.ref() != b;
+}
+
+template <typename T, typename V>
+bool operator ==(T const& a, optional<V> const& b ) {
+    return b.get() && b.ref() == a;
+}
+
+template <typename T, typename V>
+bool operator !=(T const& a, optional<V> const& b ) {
+    return !b.get() || b.ref() != a;
+
+}
+
+//
 // future & async
 //
 
 template <>
 class future<void> {
     struct future_state {
+        bool resolved;
         std::function<void(void)> consume;
     };
     std::shared_ptr<future_state> state;
@@ -46,15 +129,16 @@ public:
     future(): state(new future_state() ) { }
 
     template <typename F, typename TT>
-    void set_result(F f, TT t) const
+    void consume(F f, TT t) const
     {
         f(t);
-        set_result();
+        consume();
     }
-    void set_result() const {
-        // DOUT("future::set_result<void>");
+    void consume() const {
+        assert(!state->resolved);
+
         if( !state->consume ) {
-            // DOUT("future::set_result<void> -> abandoning");
+            state->resolved = true;
             return;
         }
         std::shared_ptr<future_state> cstate(state);
@@ -64,13 +148,26 @@ public:
         });
     }
 
+    // I emit void
+    // f shall be () -> ANY
     template <typename F>
     auto then(F f) -> future<decltype(f())> {
-        future<decltype(f())> r(f);
-        // DOUT("future("<<this->state<<") -> setting consumer");
-        this->state->consume = [=]() {
-            r.set_value();
-        };
+        future<decltype(f())> r;
+
+        if( this->state->resolved ) {
+            r.consume(f());
+        } else if( this->state->consume) {
+            auto old_consume = this->state->consume;
+            this->state->consume = [=]() {
+                old_consume();
+                r.consume(f());
+            };
+        } else {
+            this->state->consume = [=]() {
+                this->state->resolved = true;
+                r.consume(f());
+            };
+        }
         return r;
     }
 };
@@ -79,6 +176,7 @@ public:
 template <typename T>
 class future {
     struct future_state {
+        optional<T> value;
         std::function<void(T)> consume;
     };
     std::shared_ptr<future_state> state;
@@ -88,14 +186,17 @@ public:
     future(): state(new future_state() ) { }
 
     template <typename F, typename TT>
-    void set_result(F f, TT t) const
+    void consume(F f, TT t) const
     {
-        set_result(f(t));
+        consume(f(t));
     }
-    void set_result(T v) const {
+    void consume(T v) const {
         // DOUT("future::set_result<T>, T=" << typeid(T).name());
+        assert( !this->state->value );
+            // TBD, second value !???
+
         if( !state->consume ) {
-            // DOUT("future::set_result<T>, T=" << typeid(T).name() <<" -> abandoning");
+            this->state->value.reset(v);
             return;
         }
 
@@ -107,15 +208,27 @@ public:
         });
     }
 
+    // I emit T
+    // f shall be (T) -> ANY
     template <typename F>
     auto then(F f) -> future<decltype(f(dummy))> {
         using NT = decltype(f(dummy));
         future<NT> r;
         // DOUT("future T=" << typeid(T).name() << "("<<this->state<<") -> setting consumer");
-        this->state->consume = [=](T v) {
-            // r.set_result(f(v));
-            r.set_result(f, v);
-        };
+        if( this->state->value ) {
+            r.consume(f, this->state->value.ref());
+        } else if( this->state->consume ) {
+            auto old_consume = this->state->consume;
+            this->state->consume = [=](T v) {
+                old_consume(v);
+                r.consume(f, v);
+            };
+        } else {
+            this->state->consume = [=](T v) {
+                this->state->value.reset(v);
+                r.consume(f, v);
+            };
+        }
         return r;
     }
 };
@@ -123,14 +236,14 @@ public:
 template <typename NT, typename T, typename F>
 void set_result_helper(future<NT> f, T& val, F fun)
 {
-    f.set_result(fun(val));
+    f.consume(fun(val));
 }
 
 template <typename T, typename F>
 void set_result_helper(future<void> f, T& val, F fun)
 {
     fun(val);
-    f.set_result();
+    f.consume();
 }
 
 //
@@ -169,7 +282,7 @@ future<T> async_future(F fun) {
 
     async_queue.push([=]() {
         try {
-            f.set_result( fun() );
+            f.consume( fun() );
         } catch( ... ) {
             // TBD, f.set_exception not implemented
             std::cerr << "async: exception_caught, not propagatinh, abortinh aaaa\n";
@@ -203,29 +316,39 @@ void async_quit()
 future<std::string> read_content(std::string name)
 {
     return async_future<std::string>([=]() -> std::string {
-        DOUT("#9");
+        DOUT("rr #9");
         return "trompka pompka";
     });
 }
 
 int main()
 {
-    read_content("foo")
-        .then([](std::string content) -> std::vector<std::string> {
-            DOUT("#10");
-            std::vector<std::string> r;
-            // parse content
-            return r;
-        })
-        .then([](std::vector<std::string> v) -> int {
-            DOUT("#11");
-            return v.size();
-        })
-        .then([](int v)  {
-            DOUT("#12");
-            std::cout << "foo: " << v << "\n";
-            async_quit();
-        });
+    auto fc = read_content("foo");
+
+    fc.then([](std::string content) -> std::vector<std::string> {
+        DOUT("aa #10");
+        std::vector<std::string> r;
+        r.push_back(content);
+        return r;
+    })
+    .then([](std::vector<std::string> v) -> int {
+        DOUT("aa #11");
+        return v.size();
+    })
+    .then([](int v)  {
+        DOUT("aa #12");
+        std::cout << "foo: " << v << "\n";
+        async_quit();
+    });
+
+    fc.then([](std::string content) {
+        DOUT("sc #1");
+    }).then([]() -> int {
+        DOUT("sc #2 void -> int");
+        return 666;
+    }).then([](int a) {
+        DOUT("sc #3, done");
+    });
 
     async_loop();
 }
